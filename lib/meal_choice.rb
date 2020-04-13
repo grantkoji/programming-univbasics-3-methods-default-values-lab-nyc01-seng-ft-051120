ab90c0a0d1021f708975c53e718431e2b2278f7c
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-13 20:12:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,7 @@
 
 
 def meal_choice(veg1, veg2, protein = "tofu")
+    puts "What a nutritious meal!"
     puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-puts "What a nutritious meal!"
-meal_choice(veg1, veg2, protein)
